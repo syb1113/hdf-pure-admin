8952e70675b5328047c02ea8d9478c852dba90bb
jiaoTaiLang9717 <15420399+jiaotailang9717@user.noreply.gitee.com>
2025-03-17 18:18:16 +0800

    feat:articels feature vinitialize

---
+++ src/router/modules/articlesManagement.ts
@@ -0,0 +1,30 @@
+import { $t } from "@/plugins/i18n";
+import { article } from "@/router/enums";
+
+export default {
+  path: "/ArticlesManagement",
+  redirect: "/ArticlesManagement/articleType",
+  meta: {
+    icon: "ic:baseline-import-contacts",
+    title: $t("menus.pureAticleManage"),
+    rank: article
+  },
+  children: [
+    {
+      path: "/ArticlesManagement/articleType",
+      name: "articleType",
+      component: () => import("@/views/ArticlesManagement/articleType.vue"),
+      meta: {
+        title: $t("menus.pureAticleType")
+      }
+    },
+    {
+      path: "/ArticlesManagement/articlesList",
+      name: "articlesList",
+      component: () => import("@/views/ArticlesManagement/articlesList.vue"),
+      meta: {
+        title: $t("menus.pureAticleList")
+      }
+    }
+  ]
+} satisfies RouteConfigsTable;
